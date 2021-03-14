2428c25a0e6071728e3ce986996d1ddcf8b18752
Sporego <contact@filbert.io>
2021-03-14 14:58:30 -0400

    final version with comments

--- Reverse-Linked-List.js
+++ Reverse-Linked-List.js
@@ -14,10 +14,28 @@
  */
 
 class ListNode {
-  constructor(value, next) {
-    this.value = val === undefined ? 0 : value;
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
     this.next = next === undefined ? null : next;
   }
 }
 
-var reverseList = function (head) {};
+var reverseList = function (head) {
+  if (head === null) {
+    return null;
+  }
+
+  //     Declarations `prev` and `curr` are in perspective of original list
+  let prev = null;
+  let curr = head;
+
+  let reversedList; // Early declaration because of scope limitations: We cannot access the data inside the while block from outside the block
+
+  while (curr !== null) {
+    let tempVal = curr.val;
+    reversedList = new ListNode(curr.val, prev);
+    prev = reversedList;
+    curr = curr.next;
+  }
+  return reversedList;
+};
