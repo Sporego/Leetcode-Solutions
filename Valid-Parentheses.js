b023d6e2650db7d3dcd5a740f187fc6cf6255073
Sporego <contact@filbert.io>
2021-03-14 15:08:47 -0400

    copy intial from LeetCode

---
+++ Valid-Parentheses.js
@@ -0,0 +1,21 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  let history = [];
+  for (let i = 0; i < s.length; s++) {
+    const curr = s.charAt(i);
+    switch (s) {
+      case "(":
+        break;
+    }
+  }
+};
