f43b74fb15dae97db2defe351bd438518e0d553d
Sporego <contact@filbert.io>
2021-03-16 00:31:45 -0400

    touch Add-Strings.js

--- Valid-Parentheses.js
+++ Valid-Parentheses.js
@@ -9,13 +9,37 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {
+
+function returnCloser(opener) {
+  switch (opener) {
+    case "(":
+      return ")";
+    case "{":
+      return "}";
+    case "[":
+      return "]";
+  }
+}
+
+var isValid = function isValid(s) {
   let history = [];
-  for (let i = 0; i < s.length; s++) {
+
+  for (let i = 0; i < s.length; i++) {
     const curr = s.charAt(i);
-    switch (s) {
-      case "(":
-        break;
+
+    if (curr === "(" || curr === "{" || curr === "[") {
+      history.push(returnCloser(curr));
+    } else if (curr === ")" || curr === "}" || curr === "]") {
+      let last = history.pop();
+      if (last !== curr) {
+        return false;
+      }
     }
+  }
+
+  if (history.length === 0) {
+    return true;
+  } else {
+    return false;
   }
 };
