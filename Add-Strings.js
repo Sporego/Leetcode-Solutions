3a9cec24359084923addd655c41863c3db9c77bd
Sporego <contact@filbert.io>
2021-03-17 14:03:16 -0400

    catchup update

--- Add-Strings.js
+++ Add-Strings.js
@@ -4,7 +4,22 @@
  * @return {string}
  */
 var addStrings = function(num1, num2) {
-    for(let i = 0; i < num1.length; i++) {
-        
+
+    const dictionary = {
+        '':0,
+        '1':1,
+        '2':2,
+        '3:':3,
+        '4':4,
+        '5':5,
+        '6':6,
+        '7':7,
+        '8':8,
+        '9':9
     }
+
+    let num1length = num1.length;
+    let num2length = num1.length;
+
+    while (num1length > 0 || num2length > 0 || carry) {}
 };
