75cbeae0a5d287ab3e19dcad2b03e67811e4ef29
Sporego <contact@filbert.io>
2021-03-28 15:44:44 -0400

    2.0 idea

--- Trapping-Rain-Water.js
+++ Trapping-Rain-Water.js
@@ -2,59 +2,50 @@
  * @param {number[]} height
  * @return {number}
  */
-
-class IndentData{
-    constructor(indexedLocation,height) {
-        this.indexedLocation = indexedLocation
-        this.height = height
-    }
+class Prop {
+  constructor() {
+    this.array = [1];
+    this.wasFill = true;
+  }
 }
 
- var trap = function(height) {
-
-    let indentAreaTotal = 0; //Will be returned (A.K.A Maximum Water Trapped)
-    let indentMap = new Map()
-
-    let lastMaximum = -1; //Zero works here too but I prefer -1
-    let lastEvaluatedIndentIndex = 0;
-    let prev;
-    for (let i = 0; i<height.length; i++) {
-        //loop initialization assignments
-        let currentHeight = height[i]
-    
-        if (lastMaximum < height[i]) {
-            lastMaximum = new IndentData(i, currentHeight);
-            indentMap.set(i, currentHeight);
-            lastEvaluatedIndentIndex = i;
-            prev = currentHeight;
-            continue;
-        }
-
-        if (height[i] < lastMaximum.height) {
-
-        }
-
-        if (height[i] >= lastMaximum.height) {
-            // The real calculations of Area happens here!
-            let geometricOffsetWidth = 1; //This is the resolution of the height map! Think of this a the spacial width of each segmant.
-            for (let y = 1; y <= lastMaximum.height; y++) {
-                
-                let isAir;
-                (height[i-1] < y) ? isAir=true : isAir=false;
-
-                let areaArray = [];
-                let lastSegmentationIndex = i
-                for(let x=i; x >= lastEvaluatedIndentIndex; x--) {
-
-                }
-            }
-
-            // Terminus Cleanup
-
-
-        }
-
-
-
+var trap = function (height) {
+  const twoDMap = {};
+  let maximum = 0;
+  for (let i = 0; i < height.length; i++) {
+    if (height[i] > maximum) {
+      maximum = height[i];
+    } else {
+      maximum = maximum;
     }
+
+    for (let j = maximum; j > 0; j--) {
+      let isFill = j <= height[i];
+      if (twoDMap[j] === undefined) {
+        twoDMap[j] = new Prop();
+      } else {
+        if (twoDMap[j].wasFill === isFill) {
+          twoDMap[j].array.push(twoDMap[j].array.pop() + 1);
+        } else {
+          twoDMap[j].array.push(1);
+        }
+      }
+      twoDMap[j].wasFill = isFill;
+    }
+  }
+
+  let areaTotal = 0;
+  for (const prop in twoDMap) {
+    let calcArray = twoDMap[prop].array;
+    for (let i = 1; i < calcArray.length - 1; i += 2) {
+      if (calcArray.length < 3) {
+        continue;
+      }
+
+      areaTotal += calcArray[i];
+    }
+  }
+  return areaTotal;
 };
+
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
