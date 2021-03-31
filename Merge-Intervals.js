7055fd4b4ea9d4b933bac5282705ad5545e424c3
Sporego <contact@filbert.io>
2021-03-31 00:11:43 -0400

    add final version with memory comment on line 12

--- Merge-Intervals.js
+++ Merge-Intervals.js
@@ -2,13 +2,14 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
+
 function merge(intervals) {
   intervals.sort((arr1, arr2) => arr1[0] - arr2[0]);
   let i = 0;
   while (i < intervals.length) {
     let first = intervals[i];
     let second = intervals[i + 1];
-    let mergeArr = first;
+    let mergeArr = first; //Saves 2 MB of Memory o.o
     if (second && first[1] >= second[0]) {
       mergeArr[0] = first[0] <= second[0] ? first[0] : second[0];
       mergeArr[1] = first[1] <= second[1] ? second[1] : first[1];
