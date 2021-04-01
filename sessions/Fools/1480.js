d0fca9ad024577b257e44784bb89d617c0fc10c3
Sporego <contact@filbert.io>
2021-04-01 14:52:06 -0400

    favorite solution using reducer

--- sessions/Fools/1480.js
+++ sessions/Fools/1480.js
@@ -2,13 +2,7 @@
  * @param {number[]} nums
  * @return {number[]}
  */
- var runningSum = function(nums) {
-    let i = 0
-    let sum = 0
-    while (i < nums.length) {
-        sum += nums[i]
-        nums.splice(i, 1, sum)
-        i++
-    }
-    return nums
-};
+function runningSum(nums) {
+	nums.reduce((a, c, i, arr) => (arr[i] += a));
+	return nums;
+}
