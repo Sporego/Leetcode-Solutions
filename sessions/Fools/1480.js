a24c98b13184b3f5a9874a3446ee4a09bd1984c9
Sporego <contact@filbert.io>
2021-04-01 14:41:14 -0400

    final solution two runs 1 submit

--- sessions/Fools/1480.js
+++ sessions/Fools/1480.js
@@ -3,5 +3,12 @@
  * @return {number[]}
  */
  var runningSum = function(nums) {
-    
+    let i = 0
+    let sum = 0
+    while (i < nums.length) {
+        sum += nums[i]
+        nums.splice(i, 1, sum)
+        i++
+    }
+    return nums
 };
