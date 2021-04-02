34265f8600f8fbdfdffb8f15c4061637cc19cd0e
Sporego <contact@filbert.io>
2021-04-02 18:28:35 -0400

    415a is my intial solution and 415b is my favorite solution

--- sessions/Fools/415.js
+++ sessions/Fools/415.js
@@ -3,6 +3,47 @@
  * @param {string} num2
  * @return {string}
  */
- var addStrings = function(num1, num2) {
-    
+var addStrings = function (num1, num2) {
+	let returned = '';
+	let carry = 0;
+	let num1Point = num1.length - 1;
+	let num2Point = num2.length - 1;
+	const numDic = {
+		0: 0,
+		1: 1,
+		2: 2,
+		3: 3,
+		4: 4,
+		5: 5,
+		6: 6,
+		7: 7,
+		8: 8,
+		9: 9,
+		undefined: 0,
+	};
+	while (num1Point > -1 || num2Point > -1) {
+		sum = (
+			numDic[num1[num1Point]] +
+			numDic[num2[num2Point]] +
+			numDic[carry]
+		).toString();
+		console.log(sum);
+		console.log(returned);
+		if (sum.length > 1) {
+			carry = sum[0];
+			returned = sum[1].concat(returned);
+		} else {
+			carry = 0;
+			returned = sum[0].concat(returned);
+		}
+
+		num1Point--;
+		num2Point--;
+	}
+
+	if (carry !== 0 ) {
+        returned = carry.concat(returned)
+    }
+
+	return returned;
 };
---
+++ sessions/Fools/415b.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+
+ var addStrings = function(num1, num2) {
+    let i = num1.length - 1;
+    let j = num2.length - 1;
+    let carry = 0;
+    let sum = '';
+     
+    for (;i >= 0 || j >= 0 || carry > 0;i--, j--) {
+        const digit1 = i < 0 ? 0 : num1.charAt(i) * 1;
+        const digit2 = j < 0 ? 0 : num2.charAt(j) * 1;
+        const digitsSum = digit1 + digit2 + carry;
+        console.log(digitsSum%10)
+        sum = `${digitsSum % 10}${sum}`;
+        carry = Math.floor(digitsSum / 10);
+    }
+    
+    return sum;
+};
+
+console.log(addStrings("1","9"))
