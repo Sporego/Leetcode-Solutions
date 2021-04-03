0e15f47704c74f634cc7c5eb4a7f82bd26b412fc
Sporego <contact@filbert.io>
2021-04-03 10:56:31 -0400

    q1 finished and q2 initial

---
+++ bicontest49/5705.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} coordinates
+ * @return {boolean}
+ */
+ var squareIsWhite = function(coordinates) {
+    letterDic = {
+        a: 0,
+        b: 1,
+        c: 0,
+        d: 1,
+        e: 0,
+        f: 1,
+        g: 0,
+        h: 1,
+    }
+
+
+    const type = letterDic[coordinates[0]]
+    const value = ((coordinates[1] * 1) + type)
+    return (value % 2 == 0) ? true : false
+};
