7eadc594e2e62f68426569887f99d5428980a462
Sporego <contact@filbert.io>
2021-04-03 00:26:25 -0400

    solved 53 super memory efficient :)

--- sessions/Fools/53.js
+++ sessions/Fools/53.js
@@ -2,4 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function (nums) {};
+var maxSubArray = function (nums) {
+	let maximum = -Infinity;
+	let maximumBridge = 0;
+	for (let i = 0; i < nums.length; i++) {
+		if (maximum < nums[i] + maximumBridge) {
+			maximum = nums[i] + maximumBridge;
+		}
+
+		maximumBridge =
+			maximumBridge + nums[i] > 0 ? (maximumBridge += nums[i]) : 0;
+	}
+	return maximum;
+};
+
