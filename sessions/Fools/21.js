7500d4200d3b1347b5aed20dcbda94a78667c89c
Sporego <contact@filbert.io>
2021-04-03 01:27:07 -0400

    21 recursive solution complexity is O(n+m)

--- sessions/Fools/21.js
+++ sessions/Fools/21.js
@@ -10,6 +10,14 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
- var mergeTwoLists = function(l1, l2) {
-    
+
+function mergeTwoLists(l1, l2) {
+    if (!l1 || !l2) return l1 ? l1 :l2 ;
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next,l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1,l2.next);
+        return l2;
+    }
 };
