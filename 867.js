803869d1ae681a181d4913fd7aac61e760b99d34
Sporego <contact@filbert.io>
2021-04-22 02:30:47 -0400

    867 final

--- 867.js
+++ 867.js
@@ -2,21 +2,21 @@
  * @param {number[][]} matrix
  * @return {number[][]}
  */
- var transposeMap = function (matrix) {
-    return matrix[0].map((value,index) => matrix.map(row => row[index]))
+var transposeMap = function (matrix) {
+  return matrix[0].map((value, index) => matrix.map((row) => row[index]));
 };
 
 /**
  * @param {number[][]} matrix
  * @return {number[][]}
  */
- var transpose = function (matrix) {
-    let returned = []
-   for (let i = 0; i < matrix.length;i++) {
-       for (let j= 0; j <matrix[i].length;j++) {
-           if (returned[j] == undefined) returned.push([])
-           returned[j].push(matrix[i][j])
-       }
-   }
-   return returned
+var transpose = function (matrix) {
+  let returned = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (returned[j] == undefined) returned.push([]);
+      returned[j].push(matrix[i][j]);
+    }
+  }
+  return returned;
 };
